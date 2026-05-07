74f12a1a0eac267169f0c5d7babac9fd8a77d295
frnejat <69216485+frnejat@users.noreply.github.com>
2026-05-07 14:20:33 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-window.SUPABASE_CONFIG = {
-  url: "https://naeozfdmydqnbcwnmkhx.supabase.co",
-  publishableKey: "sb_publishable_Lznb0ssWYaOtYmbyCcoQCw__pV7W9TN",
-  workspaceId: "aquatic-parasitology-main",
-  autoSync: true,
-};
